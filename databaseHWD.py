d2741e1a08bad2245732f2765f35a3308335cece
Shoaib Ahmed Siddique <43477418+shoaibsiddique@users.noreply.github.com>
2024-01-07 21:47:54 +0500

    Update databaseHWD.py

--- databaseHWD.py
+++ databaseHWD.py
@@ -7,14 +7,14 @@ def GetDataFromFirebase(JSON_DB_File:str="db-cred.json",
                         databaseURL:str = "handwash-detection-db-001.firebaseapp.com"):
     cred = credentials.Certificate(JSON_DB_File)
     Config = {
-      apiKey: "AIzaSyCqZaOX3s6zIeCwv_r73_7EX_aBCDzAxno",
-      authDomain: "handwash-detection-db-001.firebaseapp.com",
-      databaseURL: "https://handwash-detection-db-001-default-rtdb.firebaseio.com",
-      projectId: "handwash-detection-db-001",
-      storageBucket: "handwash-detection-db-001.appspot.com",
-      messagingSenderId: "857179695665",
-      appId: "1:857179695665:web:1435b9041fc8df3555c310",
-      measurementId: "G-R2S4KD4YNV"
+      "apiKey": "AIzaSyCqZaOX3s6zIeCwv_r73_7EX_aBCDzAxno",
+      "authDomain": "handwash-detection-db-001.firebaseapp.com",
+      "databaseURL": "https://handwash-detection-db-001-default-rtdb.firebaseio.com",
+      "projectId": "handwash-detection-db-001",
+      "storageBucket": "handwash-detection-db-001.appspot.com",
+      "messagingSenderId": "857179695665",
+      "appId": "1:857179695665:web:1435b9041fc8df3555c310",
+      "measurementId": "G-R2S4KD4YNV"
     }
 
     firebase_admin.initialize_app(cred, Config)
