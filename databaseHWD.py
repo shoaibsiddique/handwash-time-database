e9093a4fcfa16c1587907fc5c23ed1b48c0a589f
Shoaib Ahmed Siddique <43477418+shoaibsiddique@users.noreply.github.com>
2024-01-07 21:42:17 +0500

    Update databaseHWD.py

--- databaseHWD.py
+++ databaseHWD.py
@@ -2,19 +2,19 @@ import pandas as pd
 from firebase_admin import credentials, db
 import firebase_admin
 
-def GetDataFromFirebase(JSON_DB_File:str="database-of-hwd-durations-firebase-adminsdk-d3yd8-a4a0833a8f.json",
-                        ProjectID:str = "database-of-hwd-durations",
-                        databaseURL:str = "database-of-hwd-durations.firebaseapp.com"):
+def GetDataFromFirebase(JSON_DB_File:str="db-cred.json",
+                        ProjectID:str = "handwash-detection-db-001",
+                        databaseURL:str = "handwash-detection-db-001.firebaseapp.com"):
     cred = credentials.Certificate(JSON_DB_File)
     Config = {
-    "apiKey": "AIzaSyCHD9raTlgFp-OT64UP3wV-oIU-pXLAd54",
-    "authDomain": databaseURL,
-    "databaseURL": "https://database-of-hwd-durations-default-rtdb.firebaseio.com",
-    "projectId": ProjectID,
-    "storageBucket": "database-of-hwd-durations.appspot.com",
-    "messagingSenderId": "359816228479",
-    "appId": "1:359816228479:web:7606ca596bbb4678c2594a",
-    "measurementId": "G-ZYKYSKCF8X"
+      apiKey: "AIzaSyCqZaOX3s6zIeCwv_r73_7EX_aBCDzAxno",
+      authDomain: "handwash-detection-db-001.firebaseapp.com",
+      databaseURL: "https://handwash-detection-db-001-default-rtdb.firebaseio.com",
+      projectId: "handwash-detection-db-001",
+      storageBucket: "handwash-detection-db-001.appspot.com",
+      messagingSenderId: "857179695665",
+      appId: "1:857179695665:web:1435b9041fc8df3555c310",
+      measurementId: "G-R2S4KD4YNV"
     }
 
     firebase_admin.initialize_app(cred, Config)
