dd40172a4e117c36a6b9f3fb2a0169ddf59d4dd3
Shoaib Ahmed Siddique <43477418+shoaibsiddique@users.noreply.github.com>
2024-01-09 00:21:09 +0500

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -23,6 +23,49 @@ def get_led_state_from_firebase():
 def send_data_to_firebase(data):
     db.reference().update(data)
 
+# Function to create or get session state
+def get_session_state():
+    if 'button_clicked' not in st.session_state:
+        st.session_state.button_clicked = get_led_state_from_firebase() == 1
+
+
+# Get or create session state
+get_session_state()
+
+# Get the current LED state
+current_led_state = get_led_state_from_firebase()
+
+st.title("Time Database of Handwashing Detection System-001")
+
+# Create a button to switch the LED state
+btn_lbl = 'Switch LED ON/OFF'
+button_clicked = st.button(btn_lbl)
+
+key = "LED_VAL"
+# Update session state only if the button is clicked
+if button_clicked:
+    st.session_state.button_clicked = not st.session_state.button_clicked
+
+# Display a message based on the button state
+if st.session_state.button_clicked:
+    send_data_to_firebase({key: 1})
+    st.success("LED feature is enabled")
+    #st.markdown('<p style="color: green;">The LED is currently ON</p>', unsafe_allow_html=True)
+else:
+    # Only update the value if the button is clicked, else fetch the current state
+    if button_clicked:
+        send_data_to_firebase({key: 0})
+
+    st.warning("LED feature is disabled")
+    #st.markdown('<p style="color: red;">The LED is currently OFF</p>', unsafe_allow_html=True)
+ 
+
+# Display the current LED state fetched from Firebase
+#st.markdown(f'<p>Current LED state from RPI: {current_led_state}</p>', unsafe_allow_html=True)
+
+
+# -------------------------------------------------------------------------
+
 # Function to get the list of unique dates from the JSON keys and sort them as datetime objects
 def get_dates_list():
     dates_list = []
